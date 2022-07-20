18bfe7bb2271391f10b2654bfa31a28119eb2779
Rahul Pandita <rahulpandita@users.noreply.github.com>
2022-07-20 10:38:22 -0600

    Added comments

--- samples/cpp/Nqueens.cpp
+++ samples/cpp/Nqueens.cpp
@@ -1,4 +1,9 @@
 /*************************************************************************************************/
+/*
+The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
+Each solution contains a distinct board configuration of the n-queens' placement, where 1 and 0 both indicate a queen and an empty space, respectively.
+*/
 
 #include <bits/stdc++.h>
 #define N 4
